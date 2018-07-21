8d8eb7be5e1465c8d5fcd91711c062c43beb13c3
Vinícius Soares <squimer@gmail.com>
2018-07-21 18:03:23 -0300

    Add selectors

---
+++ App/Pokemons/PokemonsSelectors.swift
@@ -0,0 +1,31 @@
+import ReSwift
+import RxSwift
+import Services
+
+final class PokemonsSelectors: ContainerProvider {
+    private lazy var store = container.resolve(AppStore.self)
+
+    var pokemons: [Pokemon] {
+        return store.state.pokemonsState.pokemons
+    }
+
+    var isFetching: Bool {
+        return store.state.pokemonsState.isFetching
+    }
+
+    var error: Error? {
+        return store.state.pokemonsState.error
+    }
+
+    func observePokemons() -> Observable<[Pokemon]> {
+        return store.asObservable().map { $0.pokemonsState.pokemons }.distinctUntilChanged()
+    }
+
+    func observeIsFetching() -> Observable<Bool> {
+        return store.asObservable().map { $0.pokemonsState.isFetching }.distinctUntilChanged()
+    }
+
+    func observeError() -> Observable<Error?> {
+        return store.asObservable().map { $0.pokemonsState.error }
+    }
+}
--- App/Pokemons/Views/PokemonsController.swift
+++ App/Pokemons/Views/PokemonsController.swift
@@ -4,8 +4,21 @@ final class PokemonsController: LayoutController {
     override var layout: File { return R.file.pokemonsControllerXml }
 
     let actions = PokemonsActions()
+    let selectors = PokemonsSelectors()
 
     override func layoutDidLoad() {
+        bag << selectors.observePokemons().subscribeNext {
+            debugPrint("Pokemons: \($0)")
+        }
+
+        bag << selectors.observeIsFetching().subscribeNext {
+            debugPrint("IsFetching: \($0)")
+        }
+
+        bag << selectors.observeError().subscribeNext {
+            debugPrint("Error: \($0)")
+        }
+
         bag << actions.getPokemons()
     }
 }
