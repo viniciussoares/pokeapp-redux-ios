75d26369299dfeae07db866ae1b2620269fbf84a
Vinícius Soares <squimer@gmail.com>
2018-07-23 22:49:17 -0300

    Set status bar style

--- App/Pokemons/Views/PokemonsController.swift
+++ App/Pokemons/Views/PokemonsController.swift
@@ -2,6 +2,7 @@ import UIKit
 
 final class PokemonsController: LayoutController {
     override var layout: File { return R.file.pokemonsControllerXml }
+    override var preferredStatusBarStyle: UIStatusBarStyle { return .lightContent }
 
     lazy var actions = PokemonsActions()
     lazy var selectors = PokemonsSelectors()
