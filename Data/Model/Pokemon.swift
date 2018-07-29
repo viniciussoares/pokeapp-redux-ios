5ccc3ce04ad6130920b9272eee0f33c5354f3314
Vinícius Soares <squimer@gmail.com>
2018-07-29 10:58:19 -0300

    Add info about type, resistant and weaknesses

--- Data/Model/Pokemon.swift
+++ Data/Model/Pokemon.swift
@@ -1,14 +1,43 @@
 import Foundation
 
 public struct Pokemon: AutoEquatable, Decodable {
-    public let id: String
-    public let name: String
-    public let image: String
+    public var id: String
+    public var name: String
+    public var image: String
+    public var type: String = ""
+    public var resistant: String = ""
+    public var weaknesses: String = ""
+
+    enum Keys: String, CodingKey {
+        case id
+        case name
+        case image
+        case types
+        case resistant
+        case weaknesses
+    }
 
     public init(id: String, name: String, image: String) {
         self.id = id
         self.name = name
         self.image = image
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: Keys.self)
+
+        self.id = try container.decode(String.self, forKey: .id)
+        self.name = try container.decode(String.self, forKey: .name)
+        self.image = try container.decode(String.self, forKey: .image)
+
+        let typeList = try container.decodeIfPresent([String].self, forKey: .types) ?? []
+        self.type = typeList.isEmpty ? "-" : typeList.reduce("") {  "\($0), \($1)" }
+
+        let resistantList = try container.decodeIfPresent([String].self, forKey: .resistant) ?? []
+        self.resistant = resistantList.isEmpty ? "-" : resistantList.reduce("") { "\($0), \($1)" }
+
+        let weaknessesList = try container.decodeIfPresent([String].self, forKey: .weaknesses) ?? []
+        self.weaknesses = weaknessesList.isEmpty ? "-" : weaknessesList.reduce("") { "\($0), \($1)" }
     }
 }
 
